ca3347e4095588b7664ba42e1a017736a8b2b2d7
Pedro Rocha <pedrosantos.ti.web@gmail.com>
2026-04-07 17:18:09 -0300

    refactor: add spinner style to date in DateTimePicker for android

--- src/components/atoms/DatePickerInput/index.tsx
+++ src/components/atoms/DatePickerInput/index.tsx
@@ -64,7 +64,7 @@ export function DatePickerInput({
           <DateTimePicker
             value={tempDate}
             mode="date"
-            display="calendar"
+            display="spinner"
             onChange={(_, selected) => {
               setShow(false);
               if (selected) onChange(formatBrDate(selected));
